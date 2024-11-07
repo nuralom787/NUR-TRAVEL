ce10487fa06eb2891b2b3a382d00c436c040a055
nuralom787 <md9577630@gmail.com>
2024-11-07 23:46:43 +0600

    complete full site with all functionality and  fully responsive

--- Script/script.js
+++ Script/script.js
@@ -5,57 +5,87 @@ const selectedSeat = document.getElementById("selected-seat")
 // Seat Select Function.
 function SelectSeat(event) {
     const seats = parseInt(selectedSeat.innerText);
-    if (seats <= 3) {
-        const innerText = event.target.innerText;
+    const innerText = event.target.innerText;
+    const selectedInnerText = `S${innerText}`;
+    const seatSelected = document.getElementById(selectedInnerText);
+    const seatSelectedText = seatSelected?.id;
+    if (selectedInnerText === seatSelectedText) {
+        const seat = document.getElementById(innerText);
+        // seat.style.backgroundColor = "#f2f2f2";
+        // seat.style.color = "#7b7d82";
+        seat.style.removeProperty("background-color");
+        seat.style.removeProperty("color");
+        removeSelectedSeatInfo(seatSelectedText);
+        increaseLeftSeats();
+        decreaseSeatSelected();
+        calculateTotalAmount("remove seat");
+    }
+    else if (seats <= 3) {
         const seat = document.getElementById(innerText);
         seat.style.backgroundColor = "#1DD100";
         seat.style.color = "#FFFFFF";
-
-        // Call Total Amount Function.
-        calculateTotalAmount(innerText);
+        addSelectedSeatInfo(event);
+        decreaseLeftSeats();
+        increaseSeatSelected();
+        calculateTotalAmount("add seat");
     }
     else {
         return alert("You can't select more than 4 seats.")
     }
-    updateLeftSeats();
-    updateSeatSelected();
-    updateSelectedSeatInfo(event);
+    activeNextBtn();
 
 
-    // const innerText = event.target.innerText;
-    // const selectedInnerText = `S${event.target.innerText}`;
-    // const seatSelected = document.getElementById(`S${innerText}`);
-    // const seatSelectedText = seatSelected?.id;
-    // console.log(selectedInnerText, seatSelectedText)
-    // if (selectedInnerText === seatSelectedText) {
-    //     const seat = document.getElementById(innerText);
-    //     seat.style.backgroundColor = "#f2f2f2";
-    //     seat.style.color = "#7b7d82";
-    // }
-    // else {
+
+    // const seats = parseInt(selectedSeat.innerText);
+    // if (seats <= 3) {
+    //     const innerText = event.target.innerText;
     //     const seat = document.getElementById(innerText);
     //     seat.style.backgroundColor = "#1DD100";
     //     seat.style.color = "#FFFFFF";
+
+    //     // Call Total Amount Function.
+    //     calculateTotalAmount(innerText);
     // }
+    // else {
+    //     return alert("You can't select more than 4 seats.")
+    // }
+    // updateLeftSeats();
+    // updateSeatSelected();
+    // addSelectedSeatInfo(event);
 };
 
 
-// Update Left Seats Function.
-function updateLeftSeats() {
+// Decrease Left Seats Function.
+function decreaseLeftSeats() {
     const seat = parseInt(seatLeft.innerText);
     seatLeft.innerText = seat - 1;
 };
 
 
-// Update Seats Selected Function.
-function updateSeatSelected() {
+// Increase Left Seats Function.
+function increaseLeftSeats() {
+    const seat = parseInt(seatLeft.innerText);
+    seatLeft.innerText = seat + 1;
+};
+
+
+// Decrease Seats Selected Function.
+function decreaseSeatSelected() {
+    const seat = parseInt(selectedSeat.innerText);
+    selectedSeat.innerText = seat - 1;
+};
+
+
+// Increase Seats Selected Function.
+function increaseSeatSelected() {
     const seat = parseInt(selectedSeat.innerText);
     selectedSeat.innerText = seat + 1;
 };
 
 
-// Update Selected Seat Info Function.
-function updateSelectedSeatInfo(event) {
+
+// Add Selected Seat Info Function.
+function addSelectedSeatInfo(event) {
     const innerText = event.target.innerText;
     const table = document.getElementById("table");
     const tBody = document.createElement("tbody");
@@ -63,22 +93,44 @@ function updateSelectedSeatInfo(event) {
     <tr class="border-b border-[#03071233]">
         <td class="px-0 py-4 text-base text-[#03071299]">${innerText}</td>
         <td class="px-0 py-4 pl-12 text-base text-[#03071299]">Economy</td>
-        <td id="S${innerText}" class="text-right px-0 py-4 text-base text-[#03071299]">500</td>
+        <td id="S${innerText}" class="text-right px-0 py-4 text-base text-[#03071299]">550</td>
     </tr>
     `;
     table.appendChild(tBody)
 };
 
 
+// Remove Selected Seat Info Function.
+function removeSelectedSeatInfo(event) {
+    const item = document.getElementById(event)
+    item.parentNode.remove();
+};
+
+
+
 // Calculate Total Amount.
-function calculateTotalAmount() {
+function calculateTotalAmount(parameter) {
     const totalPrice = document.getElementById("total-price");
     const grandPrice = document.getElementById("grand-total");
     const priceInt = parseInt(totalPrice.innerText);
-    const newTotal = priceInt + 500;
+    let newTotal = priceInt;
+    if (parameter === "add seat") {
+        newTotal = priceInt + 550;
+    } else {
+        newTotal = priceInt - 550;
+    }
     totalPrice.innerText = newTotal;
     grandPrice.innerText = newTotal;
+    if (parseInt(totalPrice.innerText) === 2200) {
+        const couponApplyBtn = document.getElementById("coupon-apply-btn");
+        couponApplyBtn.classList.remove("btn-disabled");
+    } else {
+        const couponApplyBtn = document.getElementById("coupon-apply-btn");
+        couponApplyBtn.classList.add("btn-disabled");
+    }
 };
+
+
 
 
 // Apply Discount Coupon.
@@ -90,14 +142,36 @@ function applyCoupon() {
         const priceInt = parseInt(grandPrice.innerText);
         const discount = priceInt * 15 / 100;
         grandPrice.innerText = priceInt - discount;
+        const couponApplyBtn = document.getElementById("coupon-apply-btn");
+        couponApplyBtn.classList.add("btn-disabled");
     }
     else if (inputValue === "couple20") {
         const grandPrice = document.getElementById("grand-total");
         const priceInt = parseInt(grandPrice.innerText);
         const discount = priceInt * 20 / 100;
         grandPrice.innerText = priceInt - discount;
+        const couponApplyBtn = document.getElementById("coupon-apply-btn");
+        couponApplyBtn.classList.add("btn-disabled");
     }
     else {
         return alert("Invalid Coupon Code!!")
     }
-}
+};
+
+
+// Activate/Deactivate Next or Submit Button.
+function activeNextBtn() {
+    const grandPrice = document.getElementById("grand-total");
+    const passengerNumber = document.getElementById("passenger-number");
+    const number = passengerNumber.value;
+    const priceInt = parseInt(grandPrice.innerText);
+    const inputValue = parseInt(number);
+    if (priceInt > 0 && inputValue > 0) {
+        const nextBtn = document.getElementById("next-btn");
+        nextBtn.classList.remove("btn-disabled");
+    }
+    else {
+        const nextBtn = document.getElementById("next-btn");
+        nextBtn.classList.add("btn-disabled");
+    }
+};
